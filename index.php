76d8fa80d04926e779e7d97afa4342f67789f9fd
Talha Sajjad <talha.sajjad.software@gmail.com>
2024-07-06 19:20:56 +0500

    basic changes

--- index.php
+++ index.php
@@ -12,80 +12,83 @@ if (isset($_POST['done'])) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Countdown</title>
     <link rel="stylesheet" type="text/css" href="./index.css">
 </head>
+
 <body>
     <form method="post">
         <label for="">HOURS</label>
-        <select  name="hour">
-        <script>
-            for (let i = 1; i <= 24; i++) {
-                document.write(`<option value="${i}">${i}</option>`);
-            }
-        </script>
-    </select>
-<br>
+        <select name="hour">
+            <script>
+                for (let i = 1; i <= 24; i++) {
+                    document.write(`<option value="${i}">${i}</option>`);
+                }
+            </script>
+        </select>
+        <br>
 
-    <label for="">Min</label>
+        <label for="">Min</label>
 
         <select name="min">
-        <script>
-            for (let i = 1; i <= 60; i++) {
-                document.write(`<option value="${i}">${i}</option>`);
-            }
-        </script>
-    </select>
+            <script>
+                for (let i = 1; i <= 60; i++) {
+                    document.write(`<option value="${i}">${i}</option>`);
+                }
+            </script>
+        </select>
 
-<br>
-    <label for="">Sec</label>
+        <br>
+        <label for="">Sec</label>
 
         <select name="sec">
-        <script>
-            for (let i = 0; i <= 60; i++) {
-                document.write(`<option value="${i}">${i}</option>`);
-            }
-        </script>
-    </select>
+            <script>
+                for (let i = 0; i <= 60; i++) {
+                    document.write(`<option value="${i}">${i}</option>`);
+                }
+            </script>
+        </select>
 
-<br>
-    <label for="">Month</label>
+        <br>
+        <label for="">Month</label>
 
-        <select  name="month">
-        <script>
-            for (let i = 1; i <= 12; i++) {
-                document.write(`<option value="${i}">${i}</option>`);
-            }
-        </script>
-    </select>
+        <select name="month">
+            <script>
+                for (let i = 1; i <= 12; i++) {
+                    document.write(`<option value="${i}">${i}</option>`);
+                }
+            </script>
+        </select>
 
-<br>
-    <label for="">Day</label>
+        <br>
+        <label for="">Day</label>
 
-        <select  name="day">
-        <script>
-            for (let i = 1; i <= 31; i++) {
-                document.write(`<option value="${i}">${i}</option>`);
-            }
-        </script>
-    </select>
-<br>
-    <label for="">Year</label>
+        <select name="day">
+            <script>
+                for (let i = 1; i <= 31; i++) {
+                    document.write(`<option value="${i}">${i}</option>`);
+                }
+            </script>
+        </select>
+        <br>
+        <label for="">Year</label>
 
         <select name="year">
-        <script>
-            for (let year = 1999; year <= 2050; year++) {
-                document.write(`<option value="${year}">${year}</option>`);
-            }
-        </script>
-    </select>
+            <script>
+                for (let year = 1999; year <= 2050; year++) {
+                    document.write(`<option value="${year}">${year}</option>`);
+                }
+            </script>
+        </select>
 
-    <br>
+        <br>
         <input type="submit" name="done" value="Done" id="">
-        
+
     </form>
 </body>
+
 </html>
